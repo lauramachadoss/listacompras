f8d85519dedfc38efd0644e4847cf03e552c4800
Laura Machado <lauramachadosscontato@gamil.com>
2026-03-12 15:37:38 -0300

    Adiciona horário

--- index.js
+++ index.js
@@ -23,10 +23,20 @@ botaoAdicionar.addEventListener("click", (evento) => {
     containerItemDaLista.appendChild(nomeItem);
 
     itemDaLista.appendChild(containerItemDaLista);
+
+        const diaDaSemana = new Date().toLocaleDateString("pt-BR", {weekday: "long"});
+    const data = new Date().toLocaleDateString("pt-BR");
+    const hora = new Date().toLocaleTimeString("pt-BR", {
+        hour: "numeric",
+        minute: "numeric"
+    })
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
+    const itemData = document.createElement("p");
+    itemData.innerText = dataCompleta;
+    itemData.classList.add("texto-data");
+    itemDaLista.appendChild(itemData);
+
     listaDeCompras.appendChild(itemDaLista);
 
-    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {weekday: "long"});
-    const data = new Date().toLocaleDateString("pt-BR");
-    const dataCompleta = `${diaDaSemana} (${data})`;
     })
 
